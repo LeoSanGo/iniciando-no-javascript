4222cd3536582936af3a3ed6cdad9fc92d3bcb87
LeoSanGo <souoleo@gmail.com>
2021-07-04 07:42:17 -0300

    Exercicios inicinates JS

--- exercicios-javascript.js
+++ exercicios-javascript.js
@@ -1,5 +1,28 @@
 const vetor = [1, 6, 12, 8, 7, 13, 25];
 
-console.log(vetor.reduce((prev, acc) => {
-  return prev + acc
+console.log(vetor.reduce((acc, cur) => {
+  return acc + cur
 }, 0));
+
+const vetor2 = [1, 6, 12, 8, 7, 13, 25];
+
+console.log(vetor2
+  .filter(value => value % 2 === 0)
+  .reduce((acc, cur) => {
+  return acc + cur
+}, 0));
+
+
+const vetor3 = [1, 6, 12, 8, 7, 13, 25];
+
+console.log(vetor2
+  .filter(value => value % 2 !== 0)
+  .reduce((acc, cur) => {
+  return acc + cur
+}, 0));
+
+const vetor4 = [1, 6, 12, 8, 7, 13, 25, 1];
+
+vetor4.reduce((prev, acc) => {
+
+}, {})
