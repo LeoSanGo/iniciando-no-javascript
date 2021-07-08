2ad278b1935457b938869a8fedb279dc4a0d44c1
LeoSanGo <souoleo@gmail.com>
2021-07-08 13:35:57 -0300

    Exercicios javascript

--- exercicios-javascript.js
+++ exercicios-javascript.js
@@ -1,4 +1,4 @@
-const vetor = [1, 6, 12, 8, 7, 13, 25];
+/* const vetor = [1, 6, 12, 8, 7, 13, 25];
 
 console.log(vetor.reduce((acc, cur) => {
   return acc + cur
@@ -19,10 +19,17 @@ console.log(vetor2
   .filter(value => value % 2 !== 0)
   .reduce((acc, cur) => {
   return acc + cur
-}, 0));
+}, 0)); */
 
-const vetor4 = [1, 6, 12, 8, 7, 13, 25, 1];
+const vetor4 = [1, 6, 12, 8, 7, 13, 25, 1, 6, 13];
 
-vetor4.reduce((prev, acc) => {
+const howManyTimes = (acc, value) => {
+  if (!acc[value]) {
+    acc[value] = 0
+  }
+  acc[value] = acc[value] + 1;
+  return acc;
+}
 
-}, {})
+const result = vetor4.reduce(howManyTimes, {});
+console.log(result);
